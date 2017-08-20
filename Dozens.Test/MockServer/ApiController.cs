d7777b7f0abbeda3a937552dd505ad025043548f
jsakamoto <junichi.jp@gmail.com>
2017-08-20 22:16:14 +0900

    単体テスト - モックサーバーを ASP.NET Core MVC を使った実装に変更

---
+++ Dozens.Test/MockServer/ApiController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DozensAPI.Test.MockServer
+{
+    public class ApiController : Controller
+    {
+        private MockDozensApiServer Server { get; }
+
+        public ApiController(MockDozensApiServer server)
+        {
+            this.Server = server;
+        }
+
+        [HttpGet, Route("/api/authorize.json")]
+        public IActionResult Authentication()
+        {
+            var headers = this.Request.Headers;
+            headers["X-Auth-User"].Is("jsakamoto");
+            headers["X-Auth-Key"].Is("a8753098B73131kewt987612004957d89");
+            return Content(@"{""auth_token"":""6cfb3debbbac7d144e9eb7b701f79c2225bd6646""}", "application/json");
+        }
+
+        private void ValidateCommonHeaders()
+        {
+            var headers = this.Request.Headers;
+            headers["X-Auth-Token"].Is("6cfb3debbbac7d144e9eb7b701f79c2225bd6646");
+
+            if (this.Request.ContentLength > 0)
+                headers["Content-Type"].Is("application/json");
+        }
+
+
+        [HttpGet, Route("/api/zone.json")]
+        public object GetZones()
+        {
+            ValidateCommonHeaders();
+            if (!Server.Zones.Any()) return new object[0];
+            return new
+            {
+                domain = Server.Zones.Select(z => new { id = z.Id, name = z.Name }).ToArray()
+            };
+        }
+
+        [HttpPost, Route("/api/zone/create.json")]
+        public object CreateZone([FromBody]Data data)
+        {
+            ValidateCommonHeaders();
+            data.name.IsNotNull();
+            data.name.IsNot("");
+
+            if (Regex.IsMatch(data.name, "[,/*_;:+]"))
+            {
+                return StatusCode(400, new { code = 400, message = "There are some incorrect value.", detail = new { name = new[] { "ドメイン名は正しい形式で入力してください。" } } });
+            }
+
+            var nextZoneId = Server.Zones.DefaultIfEmpty(new DozensZone { Id = 0 }).Max(z => z.Id) + 1;
+            Server.Zones.Insert(0, new DozensZone { Id = nextZoneId, Name = data.name });
+            Server.Records.Add(nextZoneId, new List<DozensRecord>());
+
+            return GetZones();
+        }
+
+        [HttpDelete, Route("/api/zone/delete/{zoneId}.json")]
+        public object DeleteZone(int zoneId)
+        {
+            ValidateCommonHeaders();
+
+            var target = Server.Zones.Single(z => z.Id == zoneId);
+            Server.Zones.Remove(target);
+            Server.Records.Remove(target.Id);
+
+            return GetZones();
+        }
+
+        [HttpGet, Route("/api/record/{zoneName}.json")]
+        public object GetRecords(string zoneName)
+        {
+            ValidateCommonHeaders();
+            var zone = Server.Zones.Single(z => z.Name == zoneName);
+
+            var records = Server.Records[zone.Id]
+                .Select(r => new
+                {
+                    id = r.Id,
+                    name = r.Name == "" ? zone.Name : r.Name + "." + zone.Name,
+                    type = r.Type,
+                    prio = r.Prio,
+                    content = r.Content,
+                    ttl = r.TTL
+                })
+                .ToArray();
+            if (!records.Any()) return new object[0];
+            return new { record = records };
+        }
+
+        [HttpPost, Route("/api/record/update/{recordId}.json")]
+        public object UpdateRecord(int recordId, [FromBody]Data data)
+        {
+            ValidateCommonHeaders();
+            data.content.IsNotNull();
+            data.content.IsNot("");
+            data.ttl.Is(t => new[] { 60, 3600, 7200, 86400 }.Contains(t));
+
+            var target = Server.Zones
+                .Select(zone => new { zone, record = Server.Records[zone.Id].FirstOrDefault(r => r.Id == recordId) })
+                .Single(item => item.record != null);
+
+            target.record.Prio = data.prio;
+            target.record.Content = data.content;
+            target.record.TTL = data.ttl;
+
+            return GetRecords(target.zone.Name);
+        }
+
+        [HttpPost, Route("/api/record/create.json")]
+        public object CreateRecord([FromBody]Data data)
+        {
+            ValidateCommonHeaders();
+            data.domain.Is(s => !string.IsNullOrEmpty(s));
+            data.name.IsNotNull();
+            data.type.Is(s => new[] { "A", "CNAME", "MX", "TXT" }.Contains(s));
+            data.ttl.Is(t => new[] { 60, 3600, 7200, 86400 }.Contains(t));
+
+            var zone = Server.Zones.Single(z => z.Name == data.domain);
+            var records = Server.Records[zone.Id];
+            records.Add(new DozensRecord
+            {
+                Name = data.name,
+                Type = data.type,
+                Prio = data.prio,
+                Content = data.content,
+                TTL = data.ttl
+            });
+
+            return GetRecords(zone.Name);
+        }
+
+        [HttpDelete, Route("/api/record/delete/{recordId}.json")]
+        public object DeleteRecord(int recordId, [FromBody]Data data)
+        {
+            ValidateCommonHeaders();
+            data.IsNull();
+
+            var target = Server.Zones
+                .Select(zone => new { zone, record = Server.Records[zone.Id].FirstOrDefault(r => r.Id == recordId) })
+                .Single(item => item.record != null);
+
+            var records = Server.Records[target.zone.Id];
+            records.Remove(target.record);
+
+            return GetRecords(target.zone.Name);
+        }
+    }
+}
--- Dozens.Test/MockServer/MockDozensApiServer.cs
+++ Dozens.Test/MockServer/MockDozensApiServer.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Internal.Networking;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -34,10 +35,10 @@ namespace DozensAPI.Test.MockServer
         public string Url { get; protected set; }
 
         /// <summary>ゾーンのリスト</summary>
-        private List<DozensZone> Zones { get; }
+        public List<DozensZone> Zones { get; }
 
         /// <summary>ゾーンIDで区別される、ゾーンごとのレコードのリスト</summary>
-        private Dictionary<int, List<DozensRecord>> Records { get; }
+        public Dictionary<int, List<DozensRecord>> Records { get; }
 
         public MockDozensApiServer()
         {
@@ -70,7 +71,12 @@ namespace DozensAPI.Test.MockServer
                 {
                     this.Host = new WebHostBuilder()
                             .UseKestrel()
-                            .Configure(this.Configure)
+                            .ConfigureServices(svc =>
+                            {
+                                svc.AddMvc();
+                                svc.AddSingleton(_ => this);
+                            })
+                            .Configure(app => app.UseMvc())
                             .Start(this.Url);
                     return;
                 }
@@ -80,191 +86,6 @@ namespace DozensAPI.Test.MockServer
                     this.AssignUrl();
                 }
             }
-        }
-
-        private void Configure(IApplicationBuilder app)
-        {
-            Map(app, "GET", "/api/authorize", ProcAuth);
-            Map(app, "POST", "/api/zone/create", ProcCreateZone);
-            Map(app, "GET", "/api/zone", ProcGetZone);
-            Map(app, "DELETE", "/api/zone/delete/(?<zoneId>\\d+)", ProcDeleteZone);
-            Map(app, "POST", "/api/record/create", ProcCreateRecord);
-            Map(app, "GET", "/api/record/(?<zoneName>.+)", ProcGetRecord);
-            Map(app, "POST", "/api/record/update/(?<recordId>\\d+)", ProcUpdateRecord);
-            Map(app, "DELETE", "/api/record/delete/(?<recordId>\\d+)", ProcDeleteRecord);
-        }
-
-        private void Map(IApplicationBuilder app, string method, string pathMatch, Func<HttpContext, Match, Data, string> action)
-        {
-            app.MapWhen(
-                context => context.Request.Method == method && Regex.IsMatch(context.Request.Path, $"^{pathMatch}\\.json$"),
-                a =>
-                {
-                    a.Run(async (context) =>
-                    {
-                        var pathInfo = Regex.Match(context.Request.Path, $"^{pathMatch}\\.json$");
-
-                        var buff = new byte[context.Request.ContentLength ?? 0];
-                        context.Request.Body.Read(buff, 0, buff.Length);
-                        var requestJson = Encoding.UTF8.GetString(buff);
-                        var data = JsonConvert.DeserializeObject<Data>(requestJson);
-
-                        var responseJson = action(context, pathInfo, data);
-
-                        context.Response.Headers.Clear();
-                        context.Response.ContentType = "application/json";
-                        await context.Response.WriteAsync(responseJson);
-                    });
-                });
-        }
-
-        private string ProcAuth(HttpContext context, Match pathInfo, Data data)
-        {
-            var headers = context.Request.Headers;
-            headers["X-Auth-User"].Is("jsakamoto");
-            headers["X-Auth-Key"].Is("a8753098B73131kewt987612004957d89");
-            return @"{""auth_token"":""6cfb3debbbac7d144e9eb7b701f79c2225bd6646""}";
-        }
-
-        private void ValidateCommonHeaders(HttpContext context)
-        {
-            var headers = context.Request.Headers;
-            headers["X-Auth-Token"].Is("6cfb3debbbac7d144e9eb7b701f79c2225bd6646");
-
-            if (context.Request.ContentLength > 0)
-                headers["Content-Type"].Is("application/json");
-        }
-
-        private string ProcGetZone(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-            return GetZonesJson();
-        }
-
-        private string GetZonesJson()
-        {
-            if (!this.Zones.Any()) return "[]";
-            return JsonConvert.SerializeObject(new
-            {
-                domain = this.Zones.Select(z => new { id = z.Id, name = z.Name }).ToArray()
-            });
-        }
-
-        private string ProcCreateZone(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-            data.name.IsNotNull();
-            data.name.IsNot("");
-
-            if (Regex.IsMatch(data.name, "[,/*_;:+]"))
-            {
-                context.Response.StatusCode = 400;
-                return "{\"code\":400,\"message\":\"There are some incorrect value.\",\"detail\":{\"name\":[\"ドメイン名は正しい形式で入力してください。\"]}}";
-            }
-
-            var nextZoneId = this.Zones.DefaultIfEmpty(new DozensZone { Id = 0 }).Max(z => z.Id) + 1;
-            this.Zones.Insert(0, new DozensZone { Id = nextZoneId, Name = data.name });
-            this.Records.Add(nextZoneId, new List<DozensRecord>());
-
-            return GetZonesJson();
-        }
-
-        private string ProcDeleteZone(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-
-            var zoneId = int.Parse(pathInfo.Groups["zoneId"].Value);
-            var target = this.Zones.Single(z => z.Id == zoneId);
-            this.Zones.Remove(target);
-            this.Records.Remove(target.Id);
-
-            return GetZonesJson();
-        }
-
-        private string ProcGetRecord(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-
-            var zoneName = pathInfo.Groups["zoneName"].Value;
-            var zone = this.Zones.Single(z => z.Name == zoneName);
-
-            return GetRecordsJson(zone);
-        }
-
-        private string GetRecordsJson(DozensZone zone)
-        {
-            var records = this.Records[zone.Id]
-                .Select(r => new
-                {
-                    id = r.Id,
-                    name = r.Name == "" ? zone.Name : r.Name + "." + zone.Name,
-                    type = r.Type,
-                    prio = r.Prio,
-                    content = r.Content,
-                    ttl = r.TTL
-                })
-                .ToArray();
-            if (!records.Any()) return "[]";
-            return JsonConvert.SerializeObject(new { record = records });
-        }
-
-        private string ProcUpdateRecord(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-            data.content.IsNotNull();
-            data.content.IsNot("");
-            data.ttl.Is(t => new[] { 60, 3600, 7200, 86400 }.Contains(t));
-
-            var recordId = int.Parse(pathInfo.Groups["recordId"].Value);
-
-            var target = this.Zones
-                .Select(zone => new { zone, record = this.Records[zone.Id].FirstOrDefault(r => r.Id == recordId) })
-                .Single(item => item.record != null);
-
-            target.record.Prio = data.prio;
-            target.record.Content = data.content;
-            target.record.TTL = data.ttl;
-
-            return GetRecordsJson(target.zone);
-        }
-
-        private string ProcCreateRecord(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-            data.domain.Is(s => !string.IsNullOrEmpty(s));
-            data.name.IsNotNull();
-            data.type.Is(s => new[] { "A", "CNAME", "MX", "TXT" }.Contains(s));
-            data.ttl.Is(t => new[] { 60, 3600, 7200, 86400 }.Contains(t));
-
-            var zone = this.Zones.Single(z => z.Name == data.domain);
-            var records = this.Records[zone.Id];
-            records.Add(new DozensRecord
-            {
-                Name = data.name,
-                Type = data.type,
-                Prio = data.prio,
-                Content = data.content,
-                TTL = data.ttl
-            });
-
-            return GetRecordsJson(zone);
-        }
-
-        private string ProcDeleteRecord(HttpContext context, Match pathInfo, Data data)
-        {
-            ValidateCommonHeaders(context);
-            data.IsNull();
-
-            var recordId = int.Parse(pathInfo.Groups["recordId"].Value);
-
-            var target = this.Zones
-                .Select(zone => new { zone, record = this.Records[zone.Id].FirstOrDefault(r => r.Id == recordId) })
-                .Single(item => item.record != null);
-
-            var records = this.Records[target.zone.Id];
-            records.Remove(target.record);
-
-            return GetRecordsJson(target.zone);
         }
 
         public void Dispose()
